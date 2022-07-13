5bfc3e0f4a5e3f8bd1f3341fd0fd002d3395a60a
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-07-12 23:00:51 -0700

    adds GenerateSW function, removes InjectManifest function

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const { InjectManifest, GenerateSW } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -25,10 +25,12 @@ module.exports = () => {
       }),
 
       // Injects our custom service worker
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
-      }),
+      // new InjectManifest({
+      //   swSrc: './src-sw.js',
+      //   swDest: 'src-sw.js',
+      // }),
+
+      new GenerateSW(),
 
       // Creates a manifest.json file.
       new WebpackPwaManifest({
